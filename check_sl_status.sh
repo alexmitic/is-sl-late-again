04617f99f82cfb365538e9adb2a6652fa00df6bf
Aleksandar Mitic <mitic.alex@hotmail.se>
2019-07-28 19:58:00 +0200

    Renamed storage file to json

--- check_sl_status.sh
+++ check_sl_status.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 function check_sl {
-    # TRAFFICTYPES=$(curl -s "http://api.sl.se/api2/trafficsituation.json?key=APIKEY" |  jq '.ResponseData?' | jq '.TrafficTypes?')
-    TRAFFICTYPES="[
-                    {
-                        \"Name\":\"Pendeltåg\", 
-                        \"StatusIcon\":\"EventGood\"
-                    }, 
-                    {   
-                        \"Name\":\"Tunnelbana\", 
-                        \"StatusIcon\":\"EventMinor\"
-                    }, 
-                    {
-                        \"Name\":\"Pendelbåt\", 
-                        \"StatusIcon\":\"EventGood\"
-                    },
-                    {
-                        \"Name\":\"Buss\", 
-                        \"StatusIcon\":\"EventGood\"
-                    }
-                  ]"
+    TRAFFICTYPES=$(curl -s "http://api.sl.se/api2/trafficsituation.json?key=" |  jq '.ResponseData?' | jq '.TrafficTypes?')
+    # TRAFFICTYPES="[
+    #                 {
+    #                     \"Name\":\"Pendeltåg\", 
+    #                     \"StatusIcon\":\"EventGood\"
+    #                 }, 
+    #                 {   
+    #                     \"Name\":\"Tunnelbana\", 
+    #                     \"StatusIcon\":\"EventMinor\"
+    #                 }, 
+    #                 {
+    #                     \"Name\":\"Pendelbåt\", 
+    #                     \"StatusIcon\":\"EventGood\"
+    #                 },
+    #                 {
+    #                     \"Name\":\"Buss\", 
+    #                     \"StatusIcon\":\"EventGood\"
+    #                 }
+    #               ]"
     LENGTH=$(jq length <<< "$TRAFFICTYPES")
 
     for (( i=0; i<$LENGTH; i++ ))
@@ -28,20 +28,20 @@ function check_sl {
         STATUS=$(jq .["$i"] <<< $TRAFFICTYPES | jq '.StatusIcon?')
         if ([ "$NAME" = "\"Buss\"" ] || [ "$NAME" = "\"Tunnelbana\"" ] || [ "$NAME" = "\"Pendeltåg\"" ]) && [ "$STATUS" != "\"EventGood\"" ]
         then
-            echo "{\"today\":1, \"total\":"$1", \"day\":"$2"}" > status.txt
+            echo "{\"today\":1, \"total\":"$1", \"day\":"$2"}" > status.json
             break
         fi
     done    
 }
 
-TODAY=$(jq '.today?' "status.txt")
-TOTAL=$(jq '.total?' "status.txt")
-DAY=$(jq '.day?' "status.txt")
+TODAY=$(jq '.today?' "status.json")
+TOTAL=$(jq '.total?' "status.json")
+DAY=$(jq '.day?' "status.json")
 DOW=$(date +%u)
 
 if [ "$DAY" != "$DOW" ] 
 then
-    echo "{\"today\":0, \"total\":"$(($TODAY + $TOTAL))", \"day\":"$DOW"}" > status.txt
+    echo "{\"today\":0, \"total\":"$(($TODAY + $TOTAL))", \"day\":"$DOW"}" > status.json
     check_sl $(($TODAY + $TOTAL)) $DOW
 elif [ "$TODAY" = 0 ] 
 then
